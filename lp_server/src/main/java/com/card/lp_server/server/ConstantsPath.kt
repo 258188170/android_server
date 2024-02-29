33dc282627596629c864dc321917a54b1403a350
mo <mothought@163.com>
2024-02-29 17:07:43 +0800

    feat: 接口请求添加完成

--- lp_server/src/main/java/com/card/lp_server/server/ConstantsPath.kt
+++ lp_server/src/main/java/com/card/lp_server/server/ConstantsPath.kt
@@ -13,6 +13,9 @@ const val GET_TYPE_LIST = "/api/getTypeList"
 /**
  * POST PATH --------------------
  */
-
 const val UPDATE_DISPLAY = "/api/update_display"
 const val COMMON_WRITE = "/api/common_write"
+const val FIND_FILE_SIZE = "/api/find_file_size"
+const val ADD_BASE_INFO = "/api/addBaseInfo"
+const val ADD_CODE_UP_REC = "/api/addCodeUpRec"
+const val ENCODE_AND_UPDATE_EINK = "/api/encodeAndUpdateEink"
